cfb67a6b58bfe0b7fc30265b593fad6419aecca6
colynascimento <colymsnascimento@gmail.com>
2024-04-12 21:03:43 -0300

    hello word

---
+++ aula001_primeiro_programa/aula001.py
@@ -0,0 +1,18 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 12/04/2024
+# Primeiro programa em Python
+
+# Importando as bibliotecas
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+# O comando print executa uma saída via terminal
+print('-' * 70)
+print('Hello Word!!!')
+print('-' * 70)
+print()
