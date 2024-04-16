07938fbd8a142d73d22ae8d0b243cd4f89ee8a98
colynascimento <colymsnascimento@gmail.com>
2024-04-16 19:59:40 -0300

    aula003 input e cont variáveis

--- aula002_variaveis/aula002.py
+++ aula002_variaveis/aula002.py
@@ -1,3 +1,10 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 15/04/2024
+# Variáveis em Python
+
+# Importando as bibliotecas (depois da importação tem que pular 2 linhas)
 import os
 
 
@@ -12,9 +19,15 @@ doador = True
 complexo = 3j  # Python trabalha diretamente com números complexos
 PI = 3.14  # Isso é uma CONSTANTE, seu valor não deve ser alterado
 
+numeros = [1, 2, 3, 4, 5]
+meses_do_ano = ('Janeiro', 'Fevereiro', 'Março')
+aluno = {'nome': 'Coly', 'idade': 24, 'nota': 10.0}
+numeros_primos = {2, 3, 5, 7, 11}
+
 # Saída utilizando o método type( para verificar o tipo da variável)
 print('-' * 70)
 print('\033[0m \033[31mTipos declarados:\033[0m')
+print('=' * 70)
 print('\033[0m A var \033[32nome \033[0m é do tipo: ', type(nome))
 print('\033[0m A var \033[32m nascimento \033[0m é do tipo: ', type(nascimento))
 print('\033[0m A var \033[32m altura \033[0m é do tipo: ', type(altura))
@@ -22,5 +35,9 @@ print('\033[0m A var \033[32m peso \033[0m é do tipo: ', type(peso))
 print('\033[0m A var \033[32m doador \033[0m é do tipo: ', type(doador))
 print('\033[0m A var \033[32m complexo \033[0m é do tipo: ', type(complexo))
 print('\033[0m A var \033[32m PI \033[0m é do tipo: ', type(PI))
+print('\033[0m A var \033[32m numeros \033[0m é do tipo: ', type(numeros))
+print('\033[0m A var \033[32m meses_do_ano \033[0m é do tipo: ', type(meses_do_ano))
+print('\033[0m A var \033[32m aluno \033[0m é do tipo: ', type(aluno))
+print('\033[0m A var \033[32m numeros_primos \033[0m é do tipo: ', type(numeros_primos))
 print('-' * 70)
 print('')
---
+++ aula003_entrada_de_dados/aula003_input.py
@@ -0,0 +1,31 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 16/04/2024
+# Entrada de Dados em Python
+
+import os
+
+# Limpar o terminal
+os.system('cls')
+
+print('-' * 70)
+print('ENTRADA DE DADOS EM PYTHON')
+print('=' * 70)
+
+# Entrada
+nome = input('Entre com um nome: ')
+nascimento = input('Data de nascimento: ')
+peso = input('Entre com o peso: ')
+altura = input('Entre com a altura: ')
+
+# Saída
+print('-' * 70)
+print('SAÍDA DE DADOS')
+print('=' * 70)
+print('Nome..............:', nome)
+print('Data de nascimento:', nascimento)
+print('Peso..............:', peso)
+print('Altura............:', altura)
+print('-' * 70)
+print('')
