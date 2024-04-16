e78cff1c17efe67e54da3fd04df39a4aab7a04ba
colynascimento <colymsnascimento@gmail.com>
2024-04-16 19:13:24 -0300

    correção variáveis

--- aula002_variaveis/aula002.py
+++ aula002_variaveis/aula002.py
@@ -13,11 +13,12 @@ complexo = 3j  # Python trabalha diretamente com números complexos
 PI = 3.14  # Isso é uma CONSTANTE, seu valor não deve ser alterado
 
 # Saída utilizando o método type( para verificar o tipo da variável)
+print('-' * 70)
 print('\033[0m \033[31mTipos declarados:\033[0m')
 print('\033[0m A var \033[32nome \033[0m é do tipo: ', type(nome))
 print('\033[0m A var \033[32m nascimento \033[0m é do tipo: ', type(nascimento))
 print('\033[0m A var \033[32m altura \033[0m é do tipo: ', type(altura))
-print('\033[0m A var \033[32m peso \033[0m é do tipo: ', type: (peso))
+print('\033[0m A var \033[32m peso \033[0m é do tipo: ', type(peso))
 print('\033[0m A var \033[32m doador \033[0m é do tipo: ', type(doador))
 print('\033[0m A var \033[32m complexo \033[0m é do tipo: ', type(complexo))
 print('\033[0m A var \033[32m PI \033[0m é do tipo: ', type(PI))
