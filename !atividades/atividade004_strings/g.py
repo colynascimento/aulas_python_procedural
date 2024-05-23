7e8e952a35cd497c487050950bee9d30a42a8af3
colynascimento <colymsnascimento@gmail.com>
2024-05-23 02:55:44 -0300

    atividade004_strings_G

--- !atividades/atividade004_strings/g.py
+++ !atividades/atividade004_strings/g.py
@@ -14,3 +14,26 @@ import os
 
 
 os.system('cls')
+
+print('-' * 70)
+print('Seja bem-vindo ao programa!')
+print('Vamos analisar um número qualquer.')
+print('=' * 70)
+
+# Entrada de dados
+numero = int(input('Insira um número inteiro: '))
+
+# Processamento
+milhares = numero // 1000
+centenas = (numero - milhares * 1000) // 100 
+dezenas = (numero - (centenas * 100 + milhares * 1000)) // 10
+unidades = (numero - (dezenas * 10 + centenas * 100 + milhares * 1000)) // 1
+
+# Saída de dados
+print('O número inserido tem: \n'
+      f'- {milhares} milhares\n'
+      f'- {centenas} centenas\n'
+      f'- {dezenas} dezenas\n'
+      f'- {unidades} unidades\n')
+
+print('-' * 70)
