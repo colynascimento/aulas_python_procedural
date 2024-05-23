aa823d12ed3b3301d30f40c863c87bdd062d58dc
colynascimento <colymsnascimento@gmail.com>
2024-05-23 20:34:30 -0300

    atividades004_strings_validaçoes

--- !atividades/atividade004_strings/i.py
+++ !atividades/atividade004_strings/i.py
@@ -20,6 +20,9 @@ print('=' * 70)
 # Entrada de dados
 nome_completo = input('Insira o seu nome completo: ')
 
+# Validação
+lista_caracteres_especiais = ['@', '#', '$', '&', '*']
+
 # Processamento
 lista_nome_completo = nome_completo.split(' ')
 primeiro_nome = lista_nome_completo[0]
@@ -27,7 +30,10 @@ indice_ultimo_nome = len(lista_nome_completo) - 1
 ultimo_nome = lista_nome_completo[indice_ultimo_nome]
 
 # Saída
-print(f'O primeiro nome é {primeiro_nome}\n'
-      f'e o último nome é {ultimo_nome}\n')
+if nome_completo in lista_nome_completo:
+      print('Os caracteres inseridos são inválidos, por favor use apenas letras.')
+else:
+      print(f'O primeiro nome é {primeiro_nome}\n'
+            f'e o último nome é {ultimo_nome}\n')
 
 print('-' * 70)
