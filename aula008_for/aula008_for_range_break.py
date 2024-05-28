4f30cc9b52866dd6d8f33eb71eb3156659e19e6b
colynascimento <colymsnascimento@gmail.com>
2024-05-28 20:30:57 -0300

    aula008_for_range_break

---
+++ aula008_for/aula008_for_range_break.py
@@ -0,0 +1,28 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 28/05/2024
+# Aula 008 - For...Range() com Validação e Casting
+
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE: BREAK')
+print('=' * 70)
+
+print()
+
+for var_iteradora in range(0, 10):
+    
+    print(f'Valor: {var_iteradora}')
+    
+    if (var_iteradora == 5):
+        print(f'Contagem interrompida no {var_iteradora}')
+        break
+    
+print('-' * 70)
+print('Fim do Programa!')
+print()
