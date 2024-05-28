25d3a3c3170d8440d5f977cae10fc4117b6143a3
colynascimento <colymsnascimento@gmail.com>
2024-05-28 19:49:50 -0300

    aula008_for_range_com_validacao_e_casting

---
+++ aula008_for/aula008_for_range_com_validacao_e_casting.py
@@ -0,0 +1,36 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 28/05/2024
+# Aula 008 - For...Range() com Validação e Casting
+
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE VALIDAÇÃO E CASTING')
+print('=' * 70)
+
+soma = 0
+
+for var_iteradora in range(0, 5):
+    
+    numero = input('Digite um número [0-5]: ')
+    
+    if (not(numero.isnumeric())):
+        print(f'"{numero}" Entrada inválida!')
+        print()
+    else:
+        numero = int(numero)
+        
+        if(numero >= 0 and numero <= 5):
+            print(f'O número {numero} está dentro do intervalo [0-5]!')
+            print()
+        else:
+            print(f'"{numero}" Entrada inválida!')
+            print()
+            
+print('-' * 70)
+print()
