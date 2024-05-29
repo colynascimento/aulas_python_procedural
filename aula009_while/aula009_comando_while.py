1a85f94f08004eae8970fd4d51743d3178039199
colynascimento <colymsnascimento@gmail.com>
2024-05-28 21:04:36 -0300

    aula009_comando_while

---
+++ aula009_while/aula009_comando_while.py
@@ -0,0 +1,27 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 28/05/2024
+# Aula 008 - Comando While
+
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE: COMANDO WHILE')
+print('=' * 70)
+
+print()
+
+contador = 1
+
+while contador <= 10:
+    print(f'contando...{contador}')
+    
+    contador += 1
+
+print('-' * 70)    
+print('Fim do programa!')
+print()
