dd871308d37fa48136da20c6e606d5d6776039e6
colynascimento <colymsnascimento@gmail.com>
2024-05-28 21:22:56 -0300

    aula009_while_else_2

---
+++ aula009_while/aula009_while_else_2.py
@@ -0,0 +1,31 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 28/05/2024
+# Aula 009 - Comando While Else
+
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE: WHILE ELSE BREAK')
+print('=' * 70)
+
+print()
+
+while(True):
+    
+    nome = str(input('Digite um nome [s - Sair]: ')).lower()
+    
+    if(nome != 's'):
+        print('Continue digitando...')
+    else:
+        print('-' * 70)
+        print('Você digitou "s" para sair!')
+        
+        break
+    
+print('-' * 70)
+print()
